69a60cf3a05aeff2e46d3d4939e4b8387072857b
Samia-Ahmad <samiiiaahmad@gmail.com>
2025-08-26 12:02:19 -0700

    home page finish

--- lib/view/screens/home/home.dart
+++ lib/view/screens/home/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,85 +7,288 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.arrow_back_ios_new, color: Colors.white),
-        title: Text('Car', style: TextStyle(color: Colors.white)),
-        centerTitle: true,
+        leading: Icon(Icons.menu),
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.location_on_outlined),
+            Text('California', style: TextStyle(color: Colors.black , fontSize: 18)),
+          ],
+        ),
+         centerTitle: true,
         actions: [
-          Icon(Icons.notifications, color: Colors.white),
-          Icon(Icons.shop_2, color: Colors.white),
-        ],
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: CircleAvatar(
+               radius: 20,
+  backgroundColor: Colors.red,
+              backgroundImage: AssetImage('assets/images/png/avatar.png')
+                    ),
+          )],
+        
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {},
-          child: Container(
-            width: MediaQuery.sizeOf(context).width,
-            margin: EdgeInsets.all(16),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
             padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.white,
+            child: Text(
+              'Browse Cars',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
+          ),
+
+          // first row under the text (Search bar)
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            child: TextField(
+              //style: TextStyle(),
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(color: Colors.grey),
+                //isDense: true, //  makes the field more compact
+                //contentPadding: EdgeInsets.symmetric(vertical: 0), // 👈 removes top padding
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black), // active color
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    print("Search icon clicked!");
+                    // TODO: implement search logic here
+                  },
+                  icon: Icon(Icons.search, color: Colors.grey),
+                ),
+              ),
+            ),
+          ),
+
+          // Second row under the text (Filter)
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text(
+                      'Filter',
+                      style: TextStyle(color: Colors.amber),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text(
+                      'Filter',
+                      style: TextStyle(color: Colors.amber),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text(
+                      'Filter',
+                      style: TextStyle(color: Colors.amber),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text(
+                      'Filter',
+                      style: TextStyle(color: Colors.amber),
+                    ),
+                  ),
+
+                  SizedBox(width: 8),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text(
+                      'Filter',
+                      style: TextStyle(color: Colors.amber),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // Third Row
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
             child: Row(
               children: [
-                Image.asset('assets/images/png/red_car.png', width: 170),
-                SizedBox(width: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('Ferrari 458 Italaia', style: TextStyle(fontSize: 24)),
-                    Row(
-                      children: [
-                        Icon(Icons.location_pin),
-                        SizedBox(width: 8),
-                        Text('New york'),
-                      ],
-                    ),
-                    Row(children: [
-                      Icon(Icons.star, color:Colors.amber),
-                      SizedBox(width:4),
-                      Text(
-                        '(129 reviews)',
-                        style: TextStyle(color:Colors.grey),
-                      ),
-                    ],
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: Colors.amber,
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          child: Row(
-                            children: [
-                              Text('\$2000'),
-                              SizedBox(width: 4),
-                              Text(
-                                'days',
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(width:8),
-                        CircleAvatar(
-                           backgroundImage: AssetImage('assets/images/png/red_car.png'),
-                        ),
-                        SizedBox(width: 8),
-                        CircleAvatar(
-                          child: Icon(Icons.favorite, color:  Colors.red ),
-                        
-                        )
-                      ],
-                    )
-                  ],
+                Text(
+                  'Available cars',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                 ),
+                Spacer(),
+                Icon(Icons.filter_alt),
               ],
             ),
+          ),
+
+          // Scrollable car list
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(8),
+              children: [
+                AvailableCarsWidget(car_name: "Ferrari 458 Italia"),
+                AvailableCarsWidget(car_name: "Toyota White"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+                AvailableCarsWidget(car_name: "Tesla S"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AvailableCarsWidget extends StatelessWidget {
+  final String car_name;
+
+  const AvailableCarsWidget({super.key, required this.car_name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          width: MediaQuery.sizeOf(context).width * 0.9,
+          margin: EdgeInsets.symmetric(vertical: 8), // add vertical spacing
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+          ),
+          child: Row(
+            children: [
+              Image.asset('assets/images/png/red_car.png', width: 170),
+              SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(car_name, style: TextStyle(fontSize: 18)),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Icon(Icons.location_pin),
+                      SizedBox(width: 8),
+                      Text('New york'),
+                    ],
+                  ),
+                  SizedBox(height: 6),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.amber),
+                      SizedBox(width: 4),
+                      Text('4.9'),
+                      SizedBox(width: 4),
+                      Text(
+                        '(129 reviews)',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 4,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              '\$2000',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              '/day',
+                              //style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      CircleAvatar(
+                        backgroundImage: AssetImage(
+                          'assets/images/png/red_car.png',
+                        ),
+                        radius: 14,
+                      ),
+                      SizedBox(width: 8),
+                      CircleAvatar(
+                        backgroundColor: Color.fromRGBO(244, 244, 244, 1),
+                        child: Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                        radius: 14,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/view/screens/settings/settings.dart
+++ lib/view/screens/settings/settings.dart
@@ -6,6 +6,7 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
         leading: Icon(Icons.arrow_back_ios_new, color: Colors.white),
@@ -21,36 +22,58 @@ class Settings extends StatelessWidget {
             topLeft: Radius.circular(24),
           ),
         ),
-        child: Column(
-           crossAxisAlignment: CrossAxisAlignment.stretch, 
-          children: [
-            SizedBox(height: 24),
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('assets/png/red car.png'),
-            ),
-            SizedBox(height: 18),
-            Text(
-              'Erica Fernandez',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                wordSpacing: 5,
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('assets/images/png/avatar.png'),
               ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Joined @jan 24, 2017',
-              style: TextStyle(color: Colors.grey, fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            settingsWidget(title: 'Settings', icon: Icons.settings,),
-            settingsWidget(title: 'Billing Details' , icon: Icons.wallet,),
-            //settingsWidget(title: 'Edit Profile',),
-            //settingsWidget(title: 'Information',),
-           // primaryButton(color: Colors.black),),
+              SizedBox(height: 18),
+              Text(
+                'Erica Fernandez',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  wordSpacing: 5,
+                ),
+              ),
 
-          ],
+              Text(
+                'Joined @jan 24, 2017',
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              settingsWidget(title: 'Settings', icon: Icons.settings),
+              SizedBox(height: 16),
+              settingsWidget(title: 'Billing Details', icon: Icons.wallet),
+              SizedBox(height: 16),
+              settingsWidget(title: 'Edit Profile', icon: Icons.edit_square),
+              SizedBox(height: 16),
+              settingsWidget(title: 'Information', icon: Icons.info),
+              // primaryButton(color: Colors.black),),
+
+              SizedBox(height: 32,),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber.withValues(alpha: 0.2),
+                      child: Icon(Icons.logout_outlined, color: Colors.amber),
+                      radius: 28,
+                    ),
+                    SizedBox(width: 16),
+                    Text(
+                      "Log out",
+                      //overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -58,31 +81,23 @@ class Settings extends StatelessWidget {
 }
 
 class primaryButton extends StatelessWidget {
-  const primaryButton({
-    super.key,
-    required this.color,
-    this.height,
-  });
-final Color color;
-final double? height;
+  const primaryButton({super.key, required this.color, this.height});
+  final Color color;
+  final double? height;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.sizeOf(context).width,
-      height: height ?? 60 ,
+      height: height ?? 60,
       color: color,
     );
   }
 }
 
 class settingsWidget extends StatelessWidget {
-  const settingsWidget({
-    super.key,
-    required this.title,
-    required this.icon,
-  });
-   final String title;
-   final IconData icon;
+  const settingsWidget({super.key, required this.title, required this.icon});
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -94,10 +109,10 @@ class settingsWidget extends StatelessWidget {
             child: Icon(icon, color: Colors.amber),
           ),
           SizedBox(width: 16),
-            Text(
-              title,
-              //overflow: TextOverflow.ellipsis,
-            ),
+          Text(
+            title,
+            //overflow: TextOverflow.ellipsis,
+          ),
           Spacer(),
           Icon(Icons.arrow_forward_ios),
         ],
